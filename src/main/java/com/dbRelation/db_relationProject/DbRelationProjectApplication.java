a8348414ccee9ccc4e3c220e35fa742658fc134c
MireilleNyoubissi <nmireillemat@yahoo.fr>
2024-04-11 07:30:33 +0100

    first commit

---
+++ src/main/java/com/dbRelation/db_relationProject/DbRelationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.dbRelation.db_relationProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbRelationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbRelationProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dbRelation/db_relationProject/DbRelationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbRelation.db_relationProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbRelationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

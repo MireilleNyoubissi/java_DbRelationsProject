8adcdf99ae8f296833c9dc4f8aa0ea60216e7025
MireilleNyoubissi <nmireillemat@yahoo.fr>
2024-04-18 06:48:46 +0100

    Created a Contact repository, added a contact and addPersonToContact dto

---
+++ src/main/java/com/dbRelation/db_relationProject/onetomany/dto/ContactDto.java
@@ -0,0 +1,10 @@
+package com.dbRelation.db_relationProject.onetomany.dto;
+
+import lombok.Data;
+
+@Data
+public class ContactDto {
+    private String name;
+    private String tel;
+    private String email;
+}

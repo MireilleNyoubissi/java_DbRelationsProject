d53843f612bfe9707306ab3034542fe6f6a5b9c7
MireilleNyoubissi <nmireillemat@yahoo.fr>
2024-04-27 07:14:32 +0100

    Created Supplier entity

--- src/main/java/com/dbRelation/db_relationProject/onetomany/entities/Order.java
+++ src/main/java/com/dbRelation/db_relationProject/onetomany/entities/Order.java
@@ -3,6 +3,8 @@ package com.dbRelation.db_relationProject.onetomany.entities;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.Set;
+
 @Entity //Tells hibernate to create a table out of this class
 @Table(name = "ORDER")
 @Data
@@ -22,6 +24,11 @@ public class Order {
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL )
     @JoinColumn(name = "employeeId")
     private Employee employee;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "ORDER_SUPPLIER", joinColumns = @JoinColumn(name = "order_id"), inverseJoinColumns = @JoinColumn(name = "supplier_id"))
+    private Set<Supplier> suppliers;
+
 
     
 }

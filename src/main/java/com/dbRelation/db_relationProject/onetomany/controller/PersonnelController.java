6a87780cde577611aaf89fe2fab2585e332c6b15
MireilleNyoubissi <nmireillemat@yahoo.fr>
2024-04-27 15:30:55 +0100

    added PutMapping to update personnel

--- src/main/java/com/dbRelation/db_relationProject/onetomany/controller/PersonnelController.java
+++ src/main/java/com/dbRelation/db_relationProject/onetomany/controller/PersonnelController.java
@@ -1,5 +1,6 @@
 package com.dbRelation.db_relationProject.onetomany.controller;
 
+import com.dbRelation.db_relationProject.onetomany.dto.CustomerDto;
 import com.dbRelation.db_relationProject.onetomany.dto.PersonnelDto;
 import com.dbRelation.db_relationProject.onetomany.entities.Customer;
 import com.dbRelation.db_relationProject.onetomany.entities.Personnel;
@@ -36,14 +37,28 @@ public class PersonnelController {
         return new ResponseEntity<>(personnel, HttpStatus.OK);
     }
 
-    //Endpoint to get a customer by id
-
+    //Endpoint to get a personnel by id
     @GetMapping("{id}")
     public ResponseEntity<?> getPersonnelById(@PathVariable Long id) {
         Optional<Personnel> personnelOptional = personnelRepository.findById(id);
         if(personnelOptional.isPresent()) {
             Personnel personnel = personnelOptional.get();
             return new ResponseEntity<>(personnel, HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Personnel doesn't exist", HttpStatus.NOT_FOUND);
+    }
+
+    //Endpoint to update a personnel
+    @PutMapping("{id}")
+    public ResponseEntity<?> updatePersonnel(@PathVariable Long id, @RequestBody PersonnelDto personnelDto) {
+        Optional<Personnel> personnelOptional = personnelRepository.findById(id);
+        if(personnelOptional.isPresent()) {
+            Personnel personnel = personnelOptional.get();
+            personnel.setFirstName(personnelDto.getFirstName());
+            personnel.setLastName(personnelDto.getLastName());
+            personnel.setBirthDate(personnelDto.getBirthDate());
+            personnel.setAge(personnelDto.getAge());
+            return new ResponseEntity<>(personnelRepository.save(personnel), HttpStatus.OK);
         }
         return new ResponseEntity<>("Personnel doesn't exist", HttpStatus.NOT_FOUND);
     }

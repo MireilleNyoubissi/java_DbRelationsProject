becffe7c3e663df91fdf33edceb08927190ef1f0
MireilleNyoubissi <nmireillemat@yahoo.fr>
2024-05-20 10:48:36 +0100

    Created login service and password encryption

--- src/main/java/com/dbRelation/db_relationProject/transaction/controller/UserController.java
+++ src/main/java/com/dbRelation/db_relationProject/transaction/controller/UserController.java
@@ -3,6 +3,8 @@ package com.dbRelation.db_relationProject.transaction.controller;
 import com.dbRelation.db_relationProject.transaction.entities.User;
 import com.dbRelation.db_relationProject.transaction.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,5 +25,11 @@ public class UserController {
     public User createUser(@RequestBody User user) {
         return userService.createUser(user);
     }
+
+    @PostMapping("/login")
+    public boolean login(@RequestBody User user) {
+        return userService.login(user);
+    }
+
 
 }
--- src/main/java/com/dbRelation/db_relationProject/transaction/services/UserService.java
+++ src/main/java/com/dbRelation/db_relationProject/transaction/services/UserService.java
@@ -4,6 +4,7 @@ import com.dbRelation.db_relationProject.transaction.entities.User;
 import com.dbRelation.db_relationProject.transaction.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -25,6 +26,27 @@ public class UserService {
         user.setPassword(passwordEncoder);
 
         return userRepository.save(user);
+    }
+
+    public User getUserByUserName(String searchUserName) {
+        return userRepository.findUserByUserName(searchUserName);
+    }
+
+    public boolean login(User user) {
+
+        //
+        User dbUser = userRepository.findUserByUserName(user.getUsername());
+        if (dbUser != null ) {
+            System.out.print("user " + dbUser.getUsername());
+            BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+            String passwordEncoder = bCryptPasswordEncoder.encode(user.getPassword());
+
+            if(bCryptPasswordEncoder.matches(user.getPassword(), dbUser.getPassword())){
+                return true;
+            }
+        }
+
+        return false;
     }
 
 
